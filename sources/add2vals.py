31005af94d224352f9f6c04e68b3016ef53daca7
brad4287 <brad4287@gmail.com>
2020-12-05 13:57:58 -0500

    updated python add and calc file

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -5,15 +5,15 @@ argnumbers = len(sys.argv) - 1
 
 if argnumbers == 2 :
     print("")
-    print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
+    print("Result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
     print("")
     sys.exit(0)
 
 if argnumbers != 2 :
     print("")
-    print("You entered " + str(argnumbers) + " value/s.")
+    print("Your entry " + str(argnumbers) + " value/s.")
     print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
+    print(" 'add2vals X Y' where X and Y are individual values.")
     print("       If add2vals is not in your path, usage is './add2vals X Y'.")
     print("       If unbundled, usage is 'python add2vals.py X Y'.")
     print("")
